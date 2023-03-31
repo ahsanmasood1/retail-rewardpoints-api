8436252f0ee11253d0c2ed4fba0a4b9230dd328f
mmasood <mmasood@expediagroup.com>
2023-03-31 15:30:37 -0500

    final commit

--- src/test/java/com/demo/api/controller/TransactionControllerTest.java
+++ src/test/java/com/demo/api/controller/TransactionControllerTest.java
@@ -1,25 +1,5 @@
 package com.demo.api.controller;
 
-import static com.demo.api.constant.AppConstants.TRANSACTIONS_SERVICE;
-import static com.demo.api.helper.TestHelper.getTransactionRequest;
-import static com.demo.api.helper.TestHelper.getTransactionResponse;
-import static org.junit.jupiter.api.Assertions.*;
-
-import com.demo.api.dto.TransactionRequest;
-import com.demo.api.service.impl.TransactionServiceImpl;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.ResponseEntity;
-
-package com.demo.api.controller;
 
 import static com.demo.api.constant.AppConstants.TRANSACTIONS_SERVICE;
 import static com.demo.api.helper.TestHelper.getTransactionRequest;
